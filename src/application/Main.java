ffa0bc04e87ccae9d1adb62a126e52f17667fddf
Modnakcarlos <modnakcarlos1@gmail.com>
2023-03-08 19:25:31 -0400

    code update!

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println();
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
